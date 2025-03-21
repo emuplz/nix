bb25c9765d5d6f965f564a6a10d65ecdedc24534
Lewis Clark <lewisc@graphcore.ai>
2025-03-21 23:52:16 +0000

    stu

--- pkgs/nvim/config/plugins/lsp/lsp.nix
+++ pkgs/nvim/config/plugins/lsp/lsp.nix
@@ -8,11 +8,7 @@
         bashls.enable = true;
         clangd.enable = true;
         nixd.enable = true;
-	rust_analyzer = {
-		enable = true;
-		installCargo = true;
-		installRustc = true;
-	};
+	rust_analyzer.enable = true;
       };
       keymaps.lspBuf = {
         "gd" = "definition";
